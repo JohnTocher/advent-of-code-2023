988992600b76e263dc2f819875abeb09dcec539e
John Tocher <jmtocher@gmail.com>
2023-12-03 16:11:45 +1100

    Uploading puzzle 3 templates and data

---
+++ Puzzle_03/puzzle_03-part_1_jmt_starter.py
@@ -0,0 +1,11 @@
+""" Advent of code 2023 - Puzzle 03
+
+    https://adventofcode.com/2023/day/3
+
+    John Tocher     
+    Starter code for puzzle 03
+"""
+
+INPUT_FILE_NAME = "puzzle_03_input_01_sample.txt"
+
+print(f"Nothing here yet - this is a template!")
---
+++ Puzzle_03/puzzle_03-part_2_jmt.py
@@ -0,0 +1,11 @@
+""" Advent of code 2023 - Puzzle 03
+
+    https://adventofcode.com/2023/day/3
+
+    John Tocher     
+    Solution to puzzle 03 part 2
+"""
+
+INPUT_FILE_NAME = "puzzle_03_input_02_sample.txt"
+
+print(f"Nothing here yet - this is a template!")
