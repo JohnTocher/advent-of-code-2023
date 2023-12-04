76832b45216764b76649608787a4e46141e3c432
John Tocher <jmtochergmail.com>
2023-12-04 15:57:45 +1100

    Uploading puzzle 04 templates

---
+++ Puzzle_04/puzzle_04_notes.md
@@ -0,0 +1,22 @@
+# Advent of Code 2023 - Puzzle 04 notes
+
+[Official link](https://adventofcode.com/2023/day/4)
+[Main page](../README.md)
+
+## Part 1
+[my solution](puzzle_04-part_1_jmt.py)
+
+TBA
+
+## Part 2
+[my solution](puzzle_04-part_2_jmt.py)
+
+TBA
+
+## Gotchas
+
+TBA
+
+# Raw text from the advent of code web site
+
+TBA
--- Puzzle_template/template_duplicator.py
+++ Puzzle_template/template_duplicator.py
@@ -27,14 +27,37 @@ def create_puzzle_folders(folder_number=1):
 
     if target_folder.is_dir():
         print(f"Folder: {target_folder} already exists!")
-        return False
+        #return False
 
     print(f"Source folder to be copied is {source_folder}")
     print(f"Target folder to be created is {target_folder}")
-    target_folder.mkdir(exist_ok=False)
+    target_folder.mkdir(exist_ok=True)
+
+    # Copy source code and markdown
+    source_ext = [".py", ".md"], ".txt"
+    source_files = (f_name for f_name in Path(source_folder).iterdir() if f_name.suffix in source_ext)
+
+    for source_file in source_files:
+        #print(f"Testing: {source_file}")
+        if "NN" in source_file.name:
+            new_name = source_file.name.replace("NN", f"{folder_number:02}")
+            dest_file = target_folder / new_name
+            print(f"Going to re-create: {source_file}")
+            print(f"Output name will be: {dest_file}")
+            with open(source_file,"r") as input_file:
+                with open(dest_file,"w") as output_file:
+                    for each_line in input_file:
+                        if "NN" in each_line:
+                            if "adventofcode.com" in each_line:
+                                new_line = each_line.replace("NN", f"{folder_number}")
+                            else:
+                                new_line = each_line.replace("NN", f"{folder_number:02}")
+                        else:
+                            new_line = each_line
+                        output_file.write(new_line)
 
     return False
 
 
 if __name__ == "__main__":
-    create_ok = create_puzzle_folders(folder_number=3)
+    create_ok = create_puzzle_folders(folder_number=4)
