133a6a024dc68177d6fbef30b68485e2e7e53ddb
John Tocher <jmtochergmail.com>
2023-12-05 10:15:39 +1100

    Added setup folder and documents

---
+++ Setup/read_me.md
@@ -0,0 +1,7 @@
+# Setup read_me
+
+This folder (Setup) is meant for configuration testing
+None of the files here are related directly to the puzzles
+
+There should be a "simple_test.py" python file you can run as test
+
---
+++ Setup/simple_test.py
@@ -0,0 +1,15 @@
+""" Simple test code
+
+    this code will do the abolsute bare minimu to check that you have a
+    functioning version of python
+
+    When run, it will print details about your python version
+    and the full path to this file
+"""
+
+import sys  # The sys module is part of the standard python library
+
+print(f"Your python version will appear below:")
+print(sys.version)
+print(f"This file is: {__file__}")
+print("All done")
